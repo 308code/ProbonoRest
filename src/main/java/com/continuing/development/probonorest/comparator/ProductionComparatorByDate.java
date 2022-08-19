9375ada73161733e8d9c0ce92543e9d8f06bd500
308code <308code@gmail.com>
2022-08-19 19:48:54 -0400

    Sort production in descending order when returning a single well in the response.

--- src/main/java/com/continuing/development/probonorest/comparator/ProductionComparatorByDate.java
+++ src/main/java/com/continuing/development/probonorest/comparator/ProductionComparatorByDate.java
@@ -1,12 +1,18 @@
 package com.continuing.development.probonorest.comparator;
 
 import com.continuing.development.probonorest.model.Production;
+import org.springframework.util.ObjectUtils;
 
 import java.util.Comparator;
 
 public class ProductionComparatorByDate implements Comparator<Production> {
     @Override
     public int compare(Production production1, Production production2) {
-        return production1.getPayedDate().compareTo(production2.getPayedDate());
+        if(!ObjectUtils.isEmpty(production1) && !ObjectUtils.isEmpty(production2) &&
+        !ObjectUtils.isEmpty(production1.getPayedDate()) && !ObjectUtils.isEmpty(production2.getPayedDate()) ) {
+            return production1.getPayedDate().compareTo(production2.getPayedDate());
+        }else{
+            return 0;
+        }
     }
 }
